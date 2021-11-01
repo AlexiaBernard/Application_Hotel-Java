4f8a0d87be6907fe5a11fe990acfeac985918007
bernardal <alexia.bernard@etu.u-pec.fr>
2021-11-01 19:22:28 +0100

    Aléxia : MP Reseration

--- src/fr/iutfbleau/projetIHM2021FI2/IHM1/MP/ReservationFactoryP.java
+++ src/fr/iutfbleau/projetIHM2021FI2/IHM1/MP/ReservationFactoryP.java
@@ -96,35 +96,70 @@ public class ReservationFactoryP implements ReservationFactory {
             ResultSet result = sql.executeQuery();
 
             //Requête pour récuperer le type de chambre de la chambre
-            PreparedStatement sql2 = this.connexion.prepareStatement("SELECT TypeChambre FROM Chambre WHERE id = ?");
-            sql2.setInt(1, c.getNumero());
-            ResultSet result2 = sql2.executeQuery();
+            sql = this.connexion.prepareStatement("SELECT TypeChambre FROM Chambre WHERE id = ?");
+            sql.setInt(1, c.getNumero());
+            ResultSet result2 = sql.executeQuery();
             if (result.getObject(1) != result2.getObject(1) ) {
                 throw new IllegalArgumentException("Erreur sur le type de la chambre: la préréservation indique " + p.getTypeChambre() + " mais la chambre est  " + c.getType());
-            } else if (//Si la chambre n'est pas disponible pour cette date){
-                throw new IllegalArgumentException("La chambre " + c.monPrint() + " n'est pas disponible pour fabriquer une réservation à partir de la préréservation " + p.monPrint());
             } else {
-                PreparedStatement sql3 = this.connexion.prepareStatement("INSERT INTO Reservation ... ");
-                //Ici il faut faire la requête d'insertion dans reservation
-                sql3.executeUpdate();
+                //Ici il faut vérifier que la chmabre mise en argument ne soit pas déjà dans une reservation
+                try {
+                    //Si la chambre n'est pas disponible pour cette date)
+                    sql = this.connexion.prepareStatement("INSERT INTO Reservation VALUES (?,?,?,?,?)");
+                    sql.setObject(1, p.getReference());
+                    sql.setObject(2, p.getDateDebut());
+                    sql.setInt(3, p.getJours());
+                    sql.setObject(4, p.getClient());
+                    sql.setInt(5, c.getNumero());
+                    //Ici il faut faire la requête d'insertion dans reservation
+                    sql.executeUpdate();
+                    sql = this.connexion.prepareStatement("DELETE FROM Prereservation WHERE reference = ?");
+                    sql.setObject(1, p.getReference());
+                    sql.executeUpdate();
+                    //Reservation reserv = new ReservationP(p.getReference(), p.getDateDebut(), p.getJours(), c, p.getClient());
+                    //return reserv;
+                    return null;
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("La chambre " + c.monPrint() + " n'est pas disponible pour fabriquer une réservation à partir de la préréservation " + p.monPrint());
+                }
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("La réservation a échoué");
         }
-        
-        else {
-            Reservation r = new ReservationNP(p.getReference(), p.getDateDebut(), p.getJours(), c, p.getClient());
-            this.addReservationToBrain(r);
-            return r;
-        }
     }
 
+    /**
+     * Cherche les réservations
+     * @param  d une date
+     * @return la ou les réservation(s) à cette date sous forme d'un ensemble
+     * @throws NullPointerException si un argument est null
+     *
+     * Ne devrait pas retourner un objet null, par contre peut être un ensemble qui est vide.
+     */ 
     @Override
     public Set<Reservation> getReservation(LocalDate d) {
-        // TODO Auto-generated method stub
-        return null;
+        Objects.requireNonNull(d,"La date proposée est null.");
+        Set<Reservation> s = new HashSet<>();
+        try {
+            PreparedStatement sql = this.connexion.prepareStatement("SELECT * FROM Reservation WHERE  = ? NOT IN (SELECT Chambre FROM Reservation ...");
+            //Requête SQL a retravailler : il faut chercher toutes les reservations de ce jour, y
+            //compris celles faites les jours précédents et toujours à la date d
+            Set<Reservation> reservations = new HashSet<>();
+            sql.setObject(1, d);
+            ResultSet result = sql.executeQuery();
+            while(result.next())
+                reservations.add((Reservation) result.getObject(1));//Creer un objet Reservation
+            return reservations;
+            //tout ça est à retravailler avec la requête : verifier chaque jour
+            //Probablement faire une boucle
+        } catch (SQLException e) {
+            throw new IllegalStateException("Problème avec la requête.");
+        }
     }
+    //CETTE METHODE EST A RETRAVAILLER
 
+
+    
     @Override
     public int getDisponibles(LocalDate d) {
         // TODO Auto-generated method stub
