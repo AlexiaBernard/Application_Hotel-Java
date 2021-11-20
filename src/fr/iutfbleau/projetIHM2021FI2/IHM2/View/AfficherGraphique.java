8a0dc33eea60887f5b2b17d20e864945bddc8e62
bernardal <alexia.bernard@etu.u-pec.fr>
2021-11-20 12:04:25 +0100

    Aléxia : modif btn + fond IHM2

--- src/fr/iutfbleau/projetIHM2021FI2/IHM2/View/AfficherGraphique.java
+++ src/fr/iutfbleau/projetIHM2021FI2/IHM2/View/AfficherGraphique.java
@@ -34,10 +34,12 @@ public class AfficherGraphique  {
         Color fond = new Color(222,212,249);
         DessinerGraphique graphique = new DessinerGraphique(this.fenetre, this.retour, 
                 this.ratio, this.ratioComp);
-        this.fenetre.remove(this.centre);
+        //this.fenetre.remove(this.centre);
+        this.centre.removeAll();
+        this.centre.add(graphique);
         graphique.setName("graphique");
         graphique.setBackground(fond);
-        this.fenetre.add(graphique, BorderLayout.CENTER);
+        //this.fenetre.add(graphique, BorderLayout.CENTER);
         this.fenetre.repaint();
         this.fenetre.revalidate();
         this.fenetre.setVisible(true);
